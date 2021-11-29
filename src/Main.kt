3136f00428b37361ebfdf0602f64ac003aa8237c
Naegon <hamelgui@gmail.com>
2021-11-29 09:24:36 +0100

    Loop in main to apply movements

--- src/Main.kt
+++ src/Main.kt
@@ -2,10 +2,20 @@ import java.io.BufferedReader
 import java.io.File
 
 fun main() {
-    val bufferedReader: BufferedReader = File("src/maps/map_01.txt").bufferedReader()
+    val bufferedReader: BufferedReader = File("src/maps/map_02.txt").bufferedReader()
     val mapData = bufferedReader.use { it.readText() }
 
     val map = Map(mapData)
 
     println(map)
+
+    var movements = map.adventurers.filter { it.getNextMove() != null }
+    do {
+        movements.forEach { it.applyNextMove(map.map) }
+        movements = map.adventurers.filter { it.getNextMove() != null }
+    } while (movements.isNotEmpty())
+
+    println("\n\n||=============== End of movements ===============||" +
+            "\n\n$map")
+
 }
